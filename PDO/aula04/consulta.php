923cefcdb7a28334d435280e6d6fde73c54ce4e7
andycaruso <almcaruso@gmail.com>
2017-08-10 09:58:14 -0300

    Versão 2.1

--- PDO/aula04/consulta.php
+++ PDO/aula04/consulta.php
@@ -4,23 +4,9 @@ require("funcoes.php");
 if(isset($_REQUEST['acao'])){
   switch ($_REQUEST['acao']) {
     case 'Sim':
-      $cod = $_REQUEST['COD'];
-      //consulta via PDO
-       $sql = "DELETE From `pessoa` where `cdpessoa`= :cod";
-        try {
-          $declaracao = $link->prepare($sql);
-          $declaracao->bindParam(':cod', $cod);
-          $declaracao->execute();
-          //rowCount é um método que retorna o número de linhas
-          //afetadas pela última chamada ao método execute
-          if ($declaracao->rowCount() > 0)
-           echo ("Pessoa excluída com sucesso!");
-          else
-            echo("Código não existe.");
-        }
-        catch (PDOException $e) {
-          print $e->getMessage();
-        }
+    $cod = $_REQUEST['COD'];
+      //rotina de exclusão 
+      require("exclui.php");
 
       break;
     case 'Alterar':
--- PDO/aula04/exclui.php
+++ PDO/aula04/exclui.php
@@ -1,25 +1,20 @@
 <?php
-require("funcoes.php");
-echo ("xxxxx");
-//dados a serem incluidos
-$cod = $_REQUEST($COD);
-
-//consulta via PDO
- $sql = "DELETE From `pessoa` where `cdpessoa`= :cod";
-  try {
-    $declaracao = $link->prepare($sql);
-    $declaracao->bindParam(':cod', $cod);
-    $declaracao->execute();
-    //rowCount é um método que retorna o número de linhas
-    //afetadas pela última chamada ao método execute
-    if ($declaracao->rowCount() > 0)
-     echo ("Pessoa excluída com sucesso!");
-    else
-      echo("Código não existe.");
-  }
-  catch (PDOException $e) {
-    print $e->getMessage();
-  }
+  //consulta via PDO
+   $sql = "DELETE From `pessoa` where `cdpessoa`= :cod";
+    try {
+      $declaracao = $link->prepare($sql);
+      $declaracao->bindParam(':cod', $cod);
+      $declaracao->execute();
+      //rowCount é um método que retorna o número de linhas
+      //afetadas pela última chamada ao método execute
+      if ($declaracao->rowCount() > 0)
+       echo ("Pessoa excluída com sucesso!");
+      else
+        echo("Código não existe.");
+    }
+    catch (PDOException $e) {
+      print $e->getMessage();
+    }
 
 
 ?>
