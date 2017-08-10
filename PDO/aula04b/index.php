79cbaac377cb5d81ad71473117c2d1ca000d2883
andycaruso <almcaruso@gmail.com>
2017-08-10 11:25:47 -0300

    Index

---
+++ PDO/aula04b/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<h2>Pessoas</h2>
+	<ul>
+		<li><a href="pessoa/inclui.php">Incluir</a></li>
+		<li><a href="pessoa/consulta.php">Consultar</a></li>
+	</ul>
+	<h2>Cidades</h2>
+	<ul>
+		<li><a href="cidade/inclui.php">Incluir</a></li>
+		<li><a href="cidade/consulta.php">Consultar</a></li>
+	</ul>
+</body>
+</html>
