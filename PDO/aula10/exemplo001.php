de6edbe9fbd6b1f12732ebe8fb9cf2e7215c5ef2
andycaruso <almcaruso@gmail.com>
2017-10-05 08:54:11 -0300

    v1.1

---
+++ PDO/aula10/exemplo001.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Introdução ao jQuery</title>
+	<meta charset="utf-8">
+	<style type="text/css">
+		div {
+			font-family:sans-serif;	
+			display:inline-block;
+			box-sizing:border-box;
+		}
+		.painel {
+			border:1px solid gray;
+			width:300px;
+			height:200px;
+			background-color:white;
+		
+		}
+		.painel_cabeca {
+			width:100%;
+			height:50px;
+			background-color:teal;	
+			display:inline-block;
+			color:white;	
+			padding:2px;
+		}
+
+		.painel_corpo {
+			width:100%;
+			height:150px;
+			padding:2px;
+		}
+
+	</style>
+</head>
+<body>
+
+	<div class="painel" id="pn1">
+		<div class="painel_cabeca">
+			Painel 1
+		</div>
+		<div class="painel_corpo">
+			Texto 1
+		</div>
+	</div>
+	<div class="painel" id="pn2">
+		<div class="painel_cabeca">
+			Painel 2
+		</div>
+		<div class="painel_corpo">
+			Texto 2
+		</div>
+	</div>
+
+	<script>
+	
+
+	</script>
+
+
+
+</body>
+</html>
