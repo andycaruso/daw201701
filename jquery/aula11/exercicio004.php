ac4eb4c79a197ee16d95c6e3768f25f5a7d1ee88
andycaruso <almcaruso@gmail.com>
2017-10-18 21:11:56 -0200

    v3

---
+++ jquery/aula11/exercicio004.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Introdução ao jQuery</title>
+<meta charset="utf-8">
+<script type="text/javascript" src="jquery/jquery-3.2.1.min.js"></script>
+<style type="text/css">
+div {
+	font-family:sans-serif;	
+	display:block;
+	box-sizing:border-box;
+}
+.painel {
+	border:1px solid gray;
+	width:250px;
+	height:200px;
+	background-color:white;
+	display:inline-block;
+}
+.painel_cabeca {
+	width:100%;
+	height:50px;
+	background-color:teal;	
+	display:inline-block;
+	color:white;	
+	padding:2px;
+}
+
+.painel_corpo {
+	width:100%;
+	height:150px;
+	padding:2px;
+}
+
+.especial {
+	background-color:orange;
+	font-size:15px;
+}
+
+</style>
+</head>
+<body>
+
+<h1>Quando o botao1 for clicado adicione a classe "especial" ao botao2</h1>
+<input type="button" id="botao1" value="Adicionar classe"><br>
+<input type="button" id="botao2" value="Botão 2">
+
+
+<script>
+	//resposta aqui
+
+</script>
+</body>
+</html>
