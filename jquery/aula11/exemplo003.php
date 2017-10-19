8bc0491297b6e5575ac963d632f2706f471c4849
andycaruso <almcaruso@gmail.com>
2017-10-19 10:35:31 -0200

    v7

--- jquery/aula11/exemplo003.php
+++ jquery/aula11/exemplo003.php
@@ -37,7 +37,7 @@
 </head>
 <body>
 <h1>Algumas funções javascript úteis para lidar com strings</h1>
-		Texto:<input type="text" id="texto" name="texto"><br>
+		Texto:<input type="text" id="texto"><br>
 		Comprimento do texto:<input type="text" id="comprimento"><br>
 		Comprimento do texto sem espaços nas extremidades:<input type="text" id="comprimento2"><hr>
 		
@@ -45,6 +45,40 @@
 	
 <script>
 $(function(){
+	$('#btn1').click(function() {
+		//guarda na variavel t o valor
+		//da caixa com id=texto
+		var t = $('#texto').val();
+		
+		//convert t em string
+		t = t.toString();
+
+		//guarda na variavel c o comprimento
+		//da string t que é obtido através
+		//da propriedade length
+		var c = t.length;
+
+		//altera o valor da caixa com
+		//id=comprimento através do método
+		//val(valor)
+		$('#comprimento').val(c);
+
+		//retira espaços do início e fim da string
+		//t através do método trim()
+		t = t.trim();
+
+		//pega novamente o comprimentp de
+		//t (agora sem espaços no inicio e fim)
+		//e guarda novamente em c
+ 		c = t.length;
+
+ 		//altera o valor da caixa com
+		//id=comprimento através do método
+		//val(valor)
+ 		$('#comprimento2').val(c);
+
+	});
+	
 
 });//fim $(function)
 
